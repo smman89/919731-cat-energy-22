f9073756d694f66c6ff9572684ca830551215382
Ewgeniy <efk.dev@gmail.com>
2021-04-26 12:51:05 +0700

    верстка декоративных элементов на странице каталога

--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,8 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--no-js');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+  navMain.classList.toggle('main-nav--closed');
 });
